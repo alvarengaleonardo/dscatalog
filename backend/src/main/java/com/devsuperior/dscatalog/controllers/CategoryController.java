450a54300840d3a84c3977ed10ee1691db4b377c
Leonardo Alvarenga <leonardoalvarengacordeiro@gmail.com>
2026-03-26 22:21:44 -0300

    DELETE Category

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -41,4 +41,10 @@ public class CategoryController {
         dto = categoryService.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
